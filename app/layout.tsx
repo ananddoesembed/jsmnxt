f88294d48efd9d510f1525b9f3f4700441e97b60
anandapadmanabhanvt <anandapadmanabhanvt@gmail.com>
2025-11-17 00:05:55 +0530

    Added assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Devflow",
-  description: "A better version of Stack overflow",
+  title: "Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
